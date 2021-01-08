0e5a1a38402bf998c31e067cd8ff311b69cefe86
Roofi ahmad Sidiq <roofiahmadsidiq@gmail.com>
2021-01-08 15:48:33 +0700

    add new campaign component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import PhotoCard from "./components/PhotoCard";
-
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useHistory,
+  Redirect,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import CampaignDetailFundraiser from "./pages/CampaignDetailFundraiser";
 
 function App() {
   return (
-    <div className="container mx-auto mt-12 px-3 lg:px-0">
-      <PhotoCard />
-      <button onClick={ () => {window.location.reload()} } className="btn-primary mb-2">Generate</button>
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route
+            exact
+            path="/fundraiser"
+            component={CampaignDetailFundraiser}
+          />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/CampaignDetailFundraiser.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import JumbotronCampaignDetail from "../components/JumbotronCampaignDetail";
+
+export default function CampaignDetailFundraiser() {
+  return (
+    <div>
+      <JumbotronCampaignDetail />
+    </div>
+  );
+}
