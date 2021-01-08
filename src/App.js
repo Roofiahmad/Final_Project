61e8a8138c0e30fb19f3770c6edf0b7ea7393b46
Jody <muhammad.adnand.jody@gmail.com>
2021-01-08 12:57:54 +0700

    rev

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import PhotoCard from "./components/PhotoCard";
+import HomePage from "./pages/HomePage";
+import { BrowserRouter as Router, Switch, Route, useHistory, Redirect } from "react-router-dom";
+
 
 
 function App() {
   return (
-    <div className="container mx-auto mt-12 px-3 lg:px-0">
-      <PhotoCard />
-      <button onClick={ () => {window.location.reload()} } className="btn-primary mb-2">Generate</button>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
