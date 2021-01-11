ded183fcd52655c4ad832e0d5afef031ae8279e7
afif-firdaus <afif.firdaus94@gmail.com>
2021-01-11 11:44:21 +0700

    update profile

--- src/components/CategoryButton.js
+++ src/components/CategoryButton.js
@@ -10,94 +10,54 @@ import sociopreneur from '../assets/sociopreneur.png';
 
 const CategoryButton = () => {
     return (
-        // <div className="container">
-        //     <h1 className="font-bold text-2xl">Find Cause you truly care about</h1>
-        //     <div className="grid grid-cols-4 gap-3 mx-auto w-4/5 items-end mt-10 md:grid-cols-8">
-        //         <div className="text-center shadow-md p-2 h-30">
-        //             <img src={disability} alt="disability" width="50" className="mx-auto"/>
-        //             <p className="pt-2 text-xs text-gray-600">Disability</p>
-        //         </div>
-        //         <div className="text-center shadow-md p-2">
-        //             <img src={disaster} alt="disaster" width="50" className="mx-auto"/>
-        //             <p className="pt-2 text-xs">Disaster</p>
-        //         </div>
-        //         <div className="text-center shadow-md p-2">
-        //             <img src={education} alt="education" width="50" className="mx-auto"/>
-        //             <p className="pt-2 text-xs">Education</p>
-        //         </div>
-        //         <div className="text-center shadow-md p-2">
-        //             <img src={environment} alt="environment" width="50" className="mx-auto"/>
-        //             <p className="pt-2 text-xs">Environment</p>
-        //         </div>
-        //         <div className="text-center shadow-md p-2">
-        //             <img src={humanity} alt="humanity" width="50" className="mx-auto"/>
-        //             <p className="pt-2 text-xs">Humanity</p>
-        //         </div>
-        //         <div className="text-center shadow-md p-2">
-        //             <img src={medical} alt="medical" width="50" className="mx-auto"/>
-        //             <p className="pt-2 text-xs">Medical</p>
-        //         </div>
-        //         <div className="text-center shadow-md p-2">
-        //             <img src={religious} alt="religious" width="50" className="mx-auto"/>
-        //             <p className="pt-2 text-xs">Religious</p>
-        //         </div>
-        //         <div className="text-center shadow-md p-2">
-        //             <img src={sociopreneur} alt="sociopreneur" width="50" className="mx-auto"/>
-        //             <p className="pt-2 text-xs">Sociopreneur</p>
-        //         </div>
-        //     </div>
-        // </div>
-
-
-
         <div className="w-10/12 mx-auto mb-32">
             <h1 className="font-bold text-2xl">Find Cause you truly care about</h1>
-            <div className="flex flex-wrap justify-center items-end mx-auto mt-14 w-11/12 md:w-8/12">
+            <div className="flex flex-wrap justify-center items-end mx-auto mt-14 w-11/12 md:w-10/12">
                 <div className="flex-shrink-0 text-center flex shadow-md h-24 w-24 m-1">
                     <a href="#" className="my-auto mx-auto">
-                        <img src={disability} alt="disability" height="40" className="mx-auto"/>
+                        <img src={disability} alt="disability" className="mx-auto h-8"/>
                         <p className="pt-2 pb-0 text-xs text-gray-700">Disability</p>
                     </a>
                 </div>
                 <div className="flex-shrink-0 text-center flex shadow-md h-24 w-24 m-1">
                     <a href="#" className="my-auto mx-auto">
-                        <img src={disaster} alt="disaster" height="50" className="mx-auto"/>
+                        <img src={disaster} alt="disaster" className="mx-auto h-8"/>
                         <p className="pt-2 pb-0 text-xs text-gray-700">Disaster</p>
                     </a>
                 </div>
                 <div className="flex-shrink-0 text-center flex shadow-md h-24 w-24 m-1">
                     <a href="#" className="my-auto mx-auto">
-                        <img src={education} alt="education" height="50" className="mx-auto"/>
+                        <img src={education} alt="education" className="mx-auto h-8"/>
                         <p className="pt-2 pb-0 text-xs text-gray-700">Education</p>
                     </a>
                 </div>
                 <div className="flex-shrink-0 text-center flex shadow-md h-24 w-24 m-1">
                     <a href="#" className="my-auto mx-auto">
-                        <img src={environment} alt="environment" height="40" className="mx-auto"/>
+                        <img src={environment} alt="environment" className="mx-auto h-8"/>
                         <p className="pt-2 pb-0 text-xs text-gray-700">Environment</p>
                     </a>
                 </div>
                 <div className="flex-shrink-0 text-center flex shadow-md h-24 w-24 m-1">
                     <a href="#" className="my-auto mx-auto">
-                        <img src={humanity} alt="humanity" height="40" className="mx-auto"/>
+                        <img src={humanity} alt="humanity" className="mx-auto h-8"/>
                         <p className="pt-2 pb-0 text-xs text-gray-700">Humanity</p>
                     </a>
                 </div>
                 <div className="flex-shrink-0 text-center flex shadow-md h-24 w-24 m-1">
                     <a href="#" className="my-auto mx-auto">
-                        <img src={medical} alt="medical" height="40" className="mx-auto"/>
+                        <img src={medical} alt="medical" className="mx-auto h-8"/>
                         <p className="pt-2 pb-0 text-xs text-gray-700">Medical</p>
                     </a>
                 </div>
                 <div className="flex-shrink-0 text-center flex shadow-md h-24 w-24 m-1">
                     <a href="#" className="my-auto mx-auto">
-                        <img src={religious} alt="religious" height="40" className="mx-auto"/>
+                        <img src={religious} alt="religious" className="mx-auto h-8"/>
                         <p className="pt-2 pb-0 text-xs text-gray-700">Religious</p>
                     </a>
                 </div>
                 <div className="flex-shrink-0 text-center flex shadow-md h-24 w-24 m-1">
                     <a href="#" className="my-auto mx-auto">
-                        <img src={sociopreneur} alt="sociopreneur" height="40" className="mx-auto"/>
+                        <img src={sociopreneur} alt="sociopreneur" className="mx-auto h-8"/>
                         <p className="pt-2 pb-0 text-xs text-gray-700">Sociopreneur</p>
                     </a>
                 </div>
