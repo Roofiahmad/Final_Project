11f9adb3f1043f9f72e29757b6161c9d2e1e9afe
Jody <muhammad.adnand.jody@gmail.com>
2021-01-14 15:46:10 +0700

    pleasee

--- src/App.js
+++ src/App.js
@@ -42,17 +42,3 @@ function App() {
 }
 
 export default App;
-
-{
-  /*function App() {
-  return (
-    <div className="container mx-auto mt-12 px-3 lg:px-0">
-      <PhotoCard />
-      <button onClick={ () => {window.location.reload()} } className="btn-primary mb-2">Generate</button>
-      <DiscoverJumbotron />
-      <CategoryButton />
-      <Newest />
-      <MostUrgent />
-      <GainedMomentum />
-  </div>*/
-}
