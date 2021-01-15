72e8e76753e77eaf68607a383cd35384c7807aa6
afif-firdaus <afif.firdaus94@gmail.com>
2021-01-15 22:07:31 +0700

    update home

--- src/components/DownloadSection.js
+++ src/components/DownloadSection.js
@@ -9,17 +9,17 @@ import playstore from "../assets/playstore.png"
 function DownloadSection() {
     return (
         <div>
-            <div className="flex flex-col sm:flex-wrap w-10/12 mt-28 mb-5 mx-auto">
+            <div className="flex flex-col mb-44 sm:flex-wrap w-10/12 mt-28 mx-auto">
                 <div className="mb-3">
                     <h1 className="font-bold text-3xl">
                         Spread kindness anytime, anywhere
                     </h1>
                 </div>
-                <div className="flex flex-wrap lg:flex-nowrap flex-row">
-                    <div className="flex lg:mr-10 h-full">
+                <div className="flex flex-wrap gap-10 space-x-6 lg:flex-nowrap flex-row">
+                    <div className="flex h-full">
                         <img src={download} alt=""/>
                     </div>
-                    <div className="flex w-1/2">
+                    <div className="flex w-full lg:w-1/2">
                         <div className="flex-col justify-between">
                             <div className="">
                                 <h1 className="text-xl font-semibold">Available Now</h1>
