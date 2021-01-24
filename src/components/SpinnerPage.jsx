d0d2cda3d2e6d6cd5bafc78d89ffe34f082e4dca
Jody <muhammad.adnand.jody@gmail.com>
2021-01-24 22:01:09 +0700

    centered spinner laod

--- src/components/SpinnerPage.jsx
+++ src/components/SpinnerPage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function SpinnerPage() {
   return (
     <div>
-      <div className="w-full h-full fixed block top-0 left-0 bg-white opacity-90 z-50 text-center text-tosca">
+      <div className="w-full h-full fixed block top-0 left-0 bg-white opacity-90 z-50 text-center text-tosca my-auto">
           <i className="fas fa-circle-notch fa-spin fa-5x"></i>
           <div>
             Tali Kasih
